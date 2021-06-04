3b15712c4f0d4faa2efa5abcfad7e7559e335e00
Gabriel Limoni <gabriel.limoni@liven.tech>
2021-06-04 12:09:30 -0300

    feat: add get shrinked url in memory repo

--- src/repository/inMemoryShrinkUrlRepository.js
+++ src/repository/inMemoryShrinkUrlRepository.js
@@ -4,5 +4,8 @@ module.exports = {
   saveShrinkedUrl: async (url, urlIdentificator) => {
     urlMap.set(urlIdentificator, url);
   },
+  getShrinkedUrl: async (urlIdentificator) => {
+    return urlMap.get(urlIdentificator);
+  },
   _urlMap: urlMap,
 };
--- src/repository/inMemoryShrinkUrlRepository.test.js
+++ src/repository/inMemoryShrinkUrlRepository.test.js
@@ -1,9 +1,22 @@
-const { _urlMap, saveShrinkedUrl } = require("./inMemoryShrinkUrlRepository");
+const {
+  _urlMap,
+  saveShrinkedUrl,
+  getShrinkedUrl,
+} = require("./inMemoryShrinkUrlRepository");
 
 describe("inMemoryShrinkUrlRepository", () => {
+  afterEach(() => {
+    _urlMap.clear();
+  });
   test("Should persist url and url identificator on memory map variable", async () => {
     await saveShrinkedUrl("any-url", "any-url-identificator");
     expect(_urlMap.size).toBe(1);
     expect(_urlMap.get("any-url-identificator")).toBe("any-url");
+  });
+
+  test("Should return correct url from identificator", async () => {
+    await saveShrinkedUrl("any-url", "any-url-identificator");
+    const foundUrl = await getShrinkedUrl("any-url-identificator");
+    expect(foundUrl).toBe("any-url");
   });
 });
